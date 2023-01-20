3429c824e0b0488db7a432c0d7e0a555e32a29e9
JimmyHernandez <jimoem24@gmail.com>
2023-01-20 17:45:00 -0400

    no spaces at start of a linet

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,7 +8,7 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *left_child = NULL;
+binary_tree_t *left_child = NULL;
 
 	if (!parent) /* check that parent is not NULL */
 		return (NULL);
